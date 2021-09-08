ffc00dd776ce94a7551baeab498a12d4ff0f3313
Timhansen1990 <89973730+Timhansen1990@users.noreply.github.com>
2021-09-08 18:18:58 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,17 +24,17 @@ public class Main {
 }
 
 
-        // 1. main() -> "no, i " //
-        // it always starts in main so from the top it prints "No, i"
-        // 2. answer below
-        // "no i, wug." //
-        // it first prints "No, i" because it is the first line of code in the main method. Afterwards it goes to the next method baffle and go through it and print "Wug" and in the last section it takes "."
-        // "You wugga wug. //
-        // then it goes back to main and check the 2nd line of code Zoop(); and this is the method in the top and the 2nd line is the text "You wugga " then it takes the next method baffle where it is the print "wug"//
-        // and then it goes to the last method ping() and print out the "."
-        // "I wug."
-        // then it goes back to main method and vhes again from after Zoop() and the next line of code is print "I, " go through to the method and takes the first line of code and print out "wug"//
-        // then it goes to the last method and print out the last line "."
+// 1. main() -> "no, i " //
+// it always starts in main so from the top it prints "No, i"
+// 2. answer below
+// "no i, wug." //
+// it first prints "No, i" because it is the first line of code in the main method. Afterwards it goes to the next method baffle and go through it and print "Wug" and in the last section it takes "."
+// "You wugga wug. //
+// then it goes back to main and check the 2nd line of code Zoop(); and this is the method in the top and the 2nd line is the text "You wugga " then it takes the next method baffle where it is the print "wug"//
+// and then it goes to the last method ping() and print out the "."
+// "I wug."//
+// then it goes back to main method and vhes again from after Zoop() and the next line of code is print "I, " go through to the method and takes the first line of code and print out "wug"//
+// then it goes to the last method and print out the last line "."
 
 
 
